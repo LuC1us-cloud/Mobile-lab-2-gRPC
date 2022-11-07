70f3369c9a610462ac85b231e19a04a41786d51e
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-11-07 12:02:36 +0200

    Renew done

--- Renew/Renew.cs
+++ Renew/Renew.cs
@@ -7,6 +7,16 @@ using Grpc.Net.Client;
 using Services;
 
 
+
+public class Book
+{
+    public int Id { get; set; } // Id (Primary key)
+    public float LoanDuration { get; set; } // LoanDuration
+    public bool Taken { get; set; } = false; // Indicates if the book is taken or not
+    public DateTime LoanTime { get; set; } // Time when the book was taken
+    public float Wear { get; set; } = 0f; // Indicates how much the book is worn
+    public float RepairPrice { get; set; } = 1f; // Price of the book per 1 wear to repair
+}
 /// <summary>
 /// Client example.
 /// </summary>
@@ -48,36 +58,76 @@ class Client
         {
             try
             {
-                //connect to the server, get service proxy
+                //connect to the server, get client proxy
                 var channel = GrpcChannel.ForAddress("http://127.0.0.1:5000");
                 var client = new Service.ServiceClient(channel);
 
-                //use service
+                //use client
                 var random = new Random();
 
                 while (true)
                 {
-                    var canAdd = client.CanAddLiquid(new Empty { }).Value;
+                    log.Info("--------------------");
+                    Thread.Sleep(10000);
 
-                    Thread.Sleep(2000);
+                    var booksRaw = client.GetWornOutBooks(new Empty { }).Books;
 
-                    var liquidToAdd = random.Next(1, 20);
-
-                    if (canAdd)
+                    if (booksRaw.Count == 0)
                     {
-                        log.Info($"Generated amount to add: {liquidToAdd}.");
-                        var addedLiquid = client.AddLiquid(new Liquid { Amount = liquidToAdd }).Value;
-                        log.Info($"Amount of liquid added: {addedLiquid}.");
-                        log.Info("\n");
+                        log.Info("No worn out books found");
+                        continue;
                     }
-                    else
-                    {
-                        log.Info("I cannot add any more liquid.");
-                        log.Info("\n");
-                    }
-                    log.Info("---");
+                    log.Info($"Found {booksRaw.Count} worn out books");
 
-                    Thread.Sleep(2000);
+                    List<Book> books = new List<Book>();
+                    foreach (var id in booksRaw)
+                    {
+                        // convert to Book
+                        var book = new Book
+                        {
+                            Id = id.Id,
+                            LoanDuration = id.LoanDuration,
+                            Taken = id.Taken,
+                            LoanTime = DateTime.Parse(id.LoanTime),
+                            Wear = id.Wear,
+                            RepairPrice = id.RepairPrice
+                        };
+                        books.Add(book);
+                    }
+
+                    float budget = client.GetLibraryBudget(new Empty { }).Budget;
+                    log.Info($"Library budget: {budget.ToString("0.00")}$");
+                    if (budget == 0)
+                    {
+                        log.Info("No budget left");
+                        continue;
+                    }
+
+                    Book mostWornOutBook = books.First();
+                    foreach (var book in books)
+                    {
+                        if (book.Wear > mostWornOutBook.Wear)
+                        {
+                            mostWornOutBook = book;
+                        }
+                    }
+
+                    log.Info($"Most worn out book: Id {mostWornOutBook.Id}, ({mostWornOutBook.Wear.ToString("0.00")})");
+
+                    float repairPrice = mostWornOutBook.RepairPrice * mostWornOutBook.Wear;
+                    float repairAmount = budget / repairPrice;
+                    float repairAmountPercentage = repairAmount * 100;
+                    log.Info($"Available repair amount: {(repairAmountPercentage).ToString("0.00")}% of {mostWornOutBook.Wear.ToString("0.00")} | Full repair price: {repairPrice.ToString("0.00")}$");
+
+                    if (repairAmountPercentage > 100) repairAmountPercentage = 100;
+
+                    float wearToRepair = mostWornOutBook.Wear * repairAmountPercentage / 100;
+
+                    log.Info($"Repairing {wearToRepair.ToString("0.00")} wear");
+
+                    client.RepairBook(new IdAndWear { Id = mostWornOutBook.Id, Wear = wearToRepair });
+
+                    Thread.Sleep(10000);
                 }
             }
             catch (Exception e)
